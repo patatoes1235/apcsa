c5dd24eae081a4876925a4b8306d256b19fa68d2
kzhou22 <kzhou12356@gmail.com>
2021-10-07 14:09:41 -0400

    rainbow lab complete

--- rainbow_lab/src/Rainbow.java
+++ rainbow_lab/src/Rainbow.java
@@ -18,28 +18,36 @@ public class Rainbow extends JPanel
 	// Draws the rainbow.
 	public void paintComponent(Graphics g)
 	{
+		System.out.println("paintComponentCalled");
 		super.paintComponent(g);
 		int width = getWidth();
 		int height = getHeight();
 		int xCenter = width / 2;
 		int yCenter = 3 * height / 4;
-		int largeRadius = 1/4 * width;
+		int largeRadius = width/4;
 		g.setColor(Color.RED);
 
 		// Draw the large semicircle:
-		g.fillArc(xCenter / 2, yCenter /2, largeRadius, largeRadius, 0, 180);
+		g.fillArc(xCenter - largeRadius/2, yCenter -largeRadius/2, largeRadius, largeRadius, 0, 180);
 
 		// Declare and initialize the radii of the small and medium
 		// semicircles and draw them:
-		// ________________________________________________
+		int smallRadius = height /4;
+		int medRadius = (int)Math.sqrt(smallRadius * largeRadius);
 
+		g.setColor(Color.GREEN);
+		g.fillArc(xCenter- medRadius/2, yCenter - medRadius/2, medRadius, medRadius, 0, 180);
+		g.setColor(Color.MAGENTA);
+		g.fillArc(xCenter-smallRadius/2, yCenter-smallRadius/2, smallRadius, smallRadius, 0, 180);
 		// Calculate the radius of the innermost (sky-color) semicircle
 		// so that the width of the middle (green) ring is the
 		// arithmetic mean of the widths of the red and magenta rings:
-		// ________________________________________________
+		int skyRadius = largeRadius - 3*medRadius + 3*smallRadius;
+		System.out.println(skyRadius);
 
 		// Draw the sky-color semicircle:
-		// ________________________________________________
+		g.setColor(skyColor);
+		g.fillArc(xCenter-skyRadius/2, yCenter-skyRadius/2, skyRadius, skyRadius, 0, 180);
 	}
 
 	public static void main(String[] args)
