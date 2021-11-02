b1834b5218cb3b33d62d1bda74330cc31656ce91
patatoes1235 <60528673+patatoes1235@users.noreply.github.com>
2021-11-02 00:49:05 -0400

    bestfit complete

--- unit4/problems/src/BestFit.java
+++ unit4/problems/src/BestFit.java
@@ -1,7 +1,17 @@
 public class BestFit {
 	public static int bestFit(int size1, int size2, int space) {
 		int i = 3;
-		if ((size1+ size2) %space >=1) return 3;
-		return 0;
+		if (size1+ size2 <= space) return i;
+		if (size1 > space && size2 > space) return 0;
+		i--;
+		if (size1 > size2) i--;
+		return i;
+	}
+	public static void main(String[] args) {
+		System.out.println("size1: " + bestFit(3, 2, 4));
+		System.out.println("size2: " + bestFit(2, 3, 4));
+		System.out.println("both: " + bestFit(1, 2, 3));
+		System.out.println("both: " + bestFit(2, 2, 6));
+		System.out.println("neither: " + bestFit(3, 3, 2));
 	}
 }
