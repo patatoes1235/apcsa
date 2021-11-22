ae530d95834eb82aa5a2447362e4e031b7ce02a3
kzhou22 <kzhou12356@gmail.com>
2021-11-22 13:47:29 -0500

    starTriangle complete

--- unit5/problems/src/StarTriangle.java
+++ unit5/problems/src/StarTriangle.java
@@ -1,5 +1,16 @@
 public class StarTriangle {
 	public static void printStarTriangle(int n) {
-
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < (n - 1); j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < 2*i + 1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
-}
+		public static void main(String[] args) {
+			printStarTriangle(7);
+		}
+	}
