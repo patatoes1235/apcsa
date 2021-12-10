eac57f258e9c5c2a9a79381860619eb6c7a93258
kzhou22 <kzhou12356@gmail.com>
2021-12-10 14:25:47 -0500

    lipogram almost done

---
+++ unit6/lipogram/src/LipogramAnalyzer.java
@@ -0,0 +1,23 @@
+public class LipogramAnalyzer {
+	private String text;
+	public LipogramAnalyzer(String text) {
+		this.text = text;
+	}
+	public String mark(char letter) {
+		String newText = text;
+		while (newText.indexOf(letter) > -1) {
+			newText = newText.replace(letter, '#');
+		}
+		return newText;
+	}
+	public String allWordsWith(char letter) {
+		String returnString = "";
+		String[] newText = text.split(" ");
+		for (int i = 0; i < newText.length; i++) {
+			if (newText[i].indexOf(letter) > -1 && !returnString.contains(newText[i])) {
+				returnString+=newText[i] + "\n";
+			}
+		}
+		return returnString;
+	}
+}
