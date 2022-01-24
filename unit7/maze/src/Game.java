ccfb6f42ec27b58b7c278ea02a4b7f00ae1d1187
kzhou22 <kzhou12356@gmail.com>
2022-01-24 14:24:31 -0500

    maze complete

--- unit7/maze/src/Game.java
+++ unit7/maze/src/Game.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
@@ -14,12 +15,22 @@ public class Game {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		map = new Map(new int[] {5,5});
-		for (int i = 0; i < 5; i++) map.spawnTreasure();
-		int treasure = 5;
-		while (true) {
+		for (int i = 0; i < 5; i++) map.spawnTreasure(false);
+		map.spawnTreasure(true);
+		System.out.println("Collect all the treasures(T) and move to the bottom right corner to win. Type your directions to continue. ");
+
+		while (map.getTreasureCounter() < 5) {
 			printMaze(map);
 			System.out.print("Direction? ");
-			map.movePlayer(input.nextLine());
+			map.movePlayer(input.nextLine().trim());
 		}
+		System.out.println("Treasures all collected! Move to the bottom right corner to exit.");
+		while (!Arrays.equals(map.getLoc(), new int[]{4,4})) {
+			printMaze(map);
+			System.out.print("Direction? ");
+			map.movePlayer(input.nextLine().trim());
+		}
+		printMaze(map);
+		System.out.println("YAYYY YOU WINNN :DDD");
 	}
 }
--- unit7/maze/src/Map.java
+++ unit7/maze/src/Map.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class Map {
 	private String[][] myMaze;
-	private static int heroRow, heroCol;
+	private static int heroRow, heroCol, treasureCounter;
+	private int[] hiddenTreasure = new int[2];
 	public Map(int[] size) {
 		heroCol = 0;
 		heroRow = 0;
+		treasureCounter = 0;
 		myMaze = new String[size[0]][size[1]];
 		for (int i = 0; i < myMaze.length; i++) {
 			for (int j = 0; j < myMaze[i].length; j++) {
@@ -13,38 +17,67 @@ public class Map {
 		myMaze[0][0] = "X";
 	}
 
-	public int[] spawnTreasure() {
+	public static int getTreasureCounter() {
+		return treasureCounter;
+	}
+
+	public int[] spawnTreasure(boolean hidden) {
 		int[] loc = new int[] {(int)(Math.random()*myMaze.length), (int)(Math.random()*myMaze[0].length)};
-		setMazeAtIndex(loc, "T");
+		if (!myMaze[loc[0]][loc[1]].equals("O")) spawnTreasure(hidden);
+		else {
+			if (hidden) hiddenTreasure = loc;
+			else setMazeAtIndex(loc, "T");
+		}
+		return loc;
 	}
 	public void setMazeAtIndex(int[] loc, String value) {
 		myMaze[loc[0]][loc[1]] = value;
+	}
+	public int[] getLoc() {
+		return new int[] {heroRow, heroCol};
 	}
 
 	public String[][] getMyMaze() {
 		return myMaze;
 	}
 
+	public boolean pickUpTreasure() {
+		if (myMaze[heroRow][heroCol].equals("T")) {
+			myMaze[heroRow][heroCol] = "X";
+			treasureCounter++;
+			return true;
+		}
+		return false;
+	}
+
 	public void movePlayer(String dir) {
-		if (dir.toLowerCase().equals("up") && heroRow !=0 && myMaze[heroRow-1][heroCol].equals("O")) {
+		if (dir.toLowerCase().equals("up") && heroRow !=0 && (myMaze[heroRow-1][heroCol].equals("O")||myMaze[heroRow-1][heroCol].equals("T"))) {
 			myMaze[heroRow][heroCol] = "O";
 			heroRow--;
+			pickUpTreasure();
 			myMaze[heroRow][heroCol] = "X";
+			if (Arrays.equals(getLoc(), hiddenTreasure)) System.out.println("YOU FOUND THE HIDDEN TREASURE!! +1000 bonus points!!!");
 		}
-		else if (dir.toLowerCase().equals("down") && heroRow != myMaze.length-1 && myMaze[heroRow+1][heroCol].equals("O")) {
+		else if (dir.toLowerCase().equals("down") && heroRow != myMaze.length-1 && (myMaze[heroRow+1][heroCol].equals("O")||myMaze[heroRow+1][heroCol].equals("T"))) {
 			myMaze[heroRow][heroCol] = "O";
 			heroRow++;
+			pickUpTreasure();
 			myMaze[heroRow][heroCol] = "X";
+			if (Arrays.equals(getLoc(), hiddenTreasure)) System.out.println("YOU FOUND THE HIDDEN TREASURE!! +1000 bonus points!!!");
 		}
-		else if (dir.toLowerCase().equals("left") && heroCol !=0 && myMaze[heroRow][heroCol-1].equals("O")) {
+		else if (dir.toLowerCase().equals("left") && heroCol !=0 && (myMaze[heroRow][heroCol-1].equals("O")||myMaze[heroRow][heroCol-1].equals("T"))) {
 			myMaze[heroRow][heroCol] = "O";
 			heroCol--;
+			pickUpTreasure();
 			myMaze[heroRow][heroCol] = "X";
+			if (Arrays.equals(getLoc(), hiddenTreasure)) System.out.println("YOU FOUND THE HIDDEN TREASURE!! +1000 bonus points!!!");
 		}
-		else if (dir.toLowerCase().equals("right") && heroCol != myMaze[myMaze.length-1].length-1 && myMaze[heroRow][heroCol+1].equals("O")) {
+		else if (dir.toLowerCase().equals("right") && heroCol != myMaze[myMaze.length-1].length-1 && (myMaze[heroRow][heroCol+1].equals("O")||myMaze[heroRow][heroCol+1].equals("T"))) {
 			myMaze[heroRow][heroCol] = "O";
 			heroCol++;
+			pickUpTreasure();
 			myMaze[heroRow][heroCol] = "X";
+			if (Arrays.equals(getLoc(), hiddenTreasure)) System.out.println("YOU FOUND THE HIDDEN TREASURE!! +1000 bonus points!!!");
 		}
 		else {
 			System.out.println("You can't go that way.");
