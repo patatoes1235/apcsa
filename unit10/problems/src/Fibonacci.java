b5c413e9133927ca0f39c974ac56321d45044c03
patatoes1235 <60528673+patatoes1235@users.noreply.github.com>
2022-03-04 00:25:20 -0500

    problems finished

--- unit10/problems/src/Fibonacci.java
+++ unit10/problems/src/Fibonacci.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 public class Fibonacci {
 	public static ArrayList<BigInteger> fibonacci(int length) {
 		ArrayList<BigInteger> fib = new ArrayList<BigInteger>(length);
-		if (length >=2) {
-			fib.add(new BigInteger();
-			for (int i = 2; i < length; i++) {
-				fib[i] = fib[i - 1] + fib[i - 2];
-			}
+		for (int i = 0; i<length && i < 2; i++) {
+			fib.add(BigInteger.valueOf(i));
+		}
+		for (int i = 2; i < length; i++) {
+			fib.add(fib.get(fib.size()-1).add(fib.get(fib.size()-2)));
+			//fib[i] = fib[i - 1] + fib[i - 2];
 		}
 		return fib;
+	}
+	public static void main(String[] args) {
+		System.out.println(fibonacci(1));
+		System.out.println(fibonacci(10));
+		System.out.println(fibonacci(101));
 	}
 }
