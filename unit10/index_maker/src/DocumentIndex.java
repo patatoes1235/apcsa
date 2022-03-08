76620c53e0fb0ac8434f429a6d28e9be54f6ab59
kzhou22 <kzhou12356@gmail.com>
2022-03-08 14:26:24 -0500

    index maker bug fixes

--- unit10/index_maker/src/DocumentIndex.java
+++ unit10/index_maker/src/DocumentIndex.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Locale;
+
 // ichumack23@belmontschools.net
 public class DocumentIndex extends ArrayList<IndexEntry> {
 
@@ -9,14 +11,35 @@ public class DocumentIndex extends ArrayList<IndexEntry> {
 		super(e);
 	}
 	public void addWord(String word, int num) {
+		if (size() ==0) {
+			add(new IndexEntry(word));
+			get(0).add(num);
+			return;
+		}
 		for (int i = 0; i< size(); i++) {
 			IndexEntry temp = get(i);
-			if (word == temp.getWord()) {
+			System.out.println(temp);
+			System.out.println(word);
+			System.out.println(temp.getWord().compareTo(word.toUpperCase()));
+			if (word.equalsIgnoreCase(temp.getWord())) {
 				temp.add(num);
+				return;
+			} else if (temp.getWord().compareTo(word.toUpperCase()) > 0) {
+				//System.out.println("ELSE STATEMENT CALLED");
+				add(i, new IndexEntry(word));
+				get(i).add(num);
 				return;
 			}
 		}
-		add(new IndexEntry(word));
-		get(size()-1).add(num);
+//		add(new IndexEntry(word));
+//		get(size() - 1).add(num);
+	}
+	public void addAllWords (String str, int num) {
+		String[] wordList = str.split("\\W+");
+		for (String word : wordList) {
+			if (!(word.equals("") || word.equals(" "))) {
+				addWord(word, num);
+			}
+		}
 	}
 }
